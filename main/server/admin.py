129d611e0250ae3a72f5efd6891f674e39221562
Valder Gallo <valdergallo@gmail.com>
2020-11-07 07:51:31 -0300

    config upload server

--- main/server/admin.py
+++ main/server/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Database
+from .models import Server
+from .models import ServerGroup
+
+
+class ServerGroupAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+
+    def save_model(self, request, obj, form, change):
+        if not obj.user:
+            obj.user = request.user
+        obj.save()
+
+
+class ServerAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "host")
+
+    def save_model(self, request, obj, form, change):
+        if not obj.user:
+            obj.user = request.user
+        obj.save()
+
+
+class DatabaseAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "host", "username", "storage_type")
+
+    def save_model(self, request, obj, form, change):
+        if not obj.user:
+            obj.user = request.user
+        obj.save()
+
+
+admin.site.register(Database, DatabaseAdmin)
+admin.site.register(Server, ServerAdmin)
+admin.site.register(ServerGroup, ServerGroupAdmin)
--- main/server/models.py
+++ main/server/models.py
@@ -13,16 +13,37 @@ class UploadServerProviderEnum(models.TextChoices):
     SFTP = "SFTP"
 
 
+class UploadStorageConfig(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, editable=False, null=True, blank=True
+    )
+    storage_type = models.CharField(
+        choices=UploadServerProviderEnum.choices, max_length=5
+    )
+    config_vars = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.storage_type
+
+
 class Database(models.Model):
     name = models.CharField(max_length=250)
     username = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
-    host = models.CharField(max_length=250)
-    ssl_key = models.FileField(upload_to="ssh_keys")
+    password = models.CharField(max_length=200, null=True, blank=True)
+    host = models.CharField(max_length=250, default="localhost")
+    ssl_key = models.FileField(upload_to="ssh_keys", null=True, blank=True)
     active_to_backup = models.BooleanField(default=True)
     active_to_retore = models.BooleanField(default=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
-    storage_type = models.CharField(choices=UploadServerProviderEnum.choices)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, editable=False, null=True, blank=True
+    )
+    storage_type = models.ForeignKey(
+        UploadStorageConfig,
+        on_delete=models.CASCADE,
+        editable=False,
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return f"{self.name} - {self.host}"
@@ -30,12 +51,26 @@ class Database(models.Model):
 
 class Server(models.Model):
     name = models.CharField(max_length=250)
-    host = models.CharField(max_length=250)
+    host = models.CharField(max_length=250, default="localhost")
     connect_username = models.CharField(max_length=150, default="ubuntu")
-    ssh_key = models.FileField(upload_to="ssh_keys")
-    ssh_key_pass = models.CharField(max_length=250)
-    databases = models.ManyToManyField(Database)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
+    ssh_key = models.FileField(upload_to="ssh_keys", null=True, blank=True)
+    ssh_key_pass = models.CharField(max_length=250, null=True, blank=True)
+    databases = models.ManyToManyField(Database, blank=True)
+    backup_dir = models.CharField(max_length=100, default="/backups/")
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, editable=False, null=True, blank=True
+    )
 
     def __str__(self):
         return f"{self.name} - {self.host}"
+
+
+class ServerGroup(models.Model):
+    name = models.CharField(max_length=250)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, editable=False, null=True, blank=True
+    )
+    servers = models.ManyToManyField(Server, blank=True)
+
+    def __str__(self):
+        return f"{self.name}"
