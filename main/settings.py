2384d8a6783716e4876d4076d550522354dce8db
Valder Gallo <valdergallo@gmail.com>
2021-08-21 10:09:53 -0300

    reset migrations for backup

--- main/settings.py
+++ main/settings.py
@@ -37,6 +37,7 @@ DEFAULT_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "reset_migrations",
 ]
 
 AVAILABLE_APPS = ["server", "backup"]
